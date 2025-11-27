f729afa6d4d3c6a2f14f2931fb6cf8535327e816
zahraufa <zahratulauliafauzir@gmail.com>
2025-11-27 17:43:17 +0700

    ngubah database dan nambahin sqlite3

--- backend-rnd/src/config/database.js
+++ backend-rnd/src/config/database.js
@@ -1,16 +1,29 @@
 import mysql from 'mysql2/promise'; 
 import 'dotenv/config'; 
+import { open } from 'sqlite';
+import fs from 'fs';
+import path from 'path';
+import sqlite3 from 'sqlite3';
 
-const dbConfig = {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD, 
-    database: process.env.DB_DATABASE, 
-    port: process.env.DB_PORT,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-};
+let db = null;
+
+const CREATE_FEEDBACK_TABLE = `
+    CREATE TABLE IF NOT EXISTS feedback (
+        id VARCHAR(255) PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        email VARCHAR(100) NOT NULL,
+        eventName VARCHAR(100) NOT NULL,
+        division TEXT CHECK(division IN ('LnT', 'EEO', 'PR', 'HRD', 'RnD')) NOT NULL,
+        rating INTEGER NOT NULL CHECK (rating BETWEEN 1 AND 5),
+        comment TEXT,
+        suggestion TEXT,
+        createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+        status TEXT CHECK(status IN ('open', 'in-review', 'resolved')) DEFAULT 'open' NOT NULL
+    );
+`;
+
+async function initDatabase() {
+    const dbPath = path.join(process.cwd(), 'backend-rnd', 'feedback.sqlite');
 
 const pool = mysql.createPool(dbConfig);
 
